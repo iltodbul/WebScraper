a3c2816f1e4cf7994ac2941cde3998f41ec78091
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2022-02-09 12:40:51 +0200

    Initial commit

---
+++ WebScraper/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace WebScraper
+{
+    using System;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
